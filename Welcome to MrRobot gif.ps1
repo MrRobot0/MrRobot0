3f3dbd20904161f4991b7eecc2a29d28851b50d7
MrRobot0 <wietze@osch.net>
2020-08-15 02:51:34 +0200

    Create Welcome to MrRobot gif.ps1

---
+++ Welcome to MrRobot gif.ps1
@@ -0,0 +1,81 @@
+﻿cls
+Start-Sleep -Seconds 2 -Verbose
+cls
+Write-Host "W"
+start-sleep -Milliseconds 150
+cls
+Write-Host "We"
+start-sleep -Milliseconds 150
+cls
+Write-Host "Wel"
+start-sleep -Milliseconds 150
+cls
+Write-Host "Welc"
+start-sleep -Milliseconds 150
+cls
+Write-Host "Welco"
+start-sleep -Milliseconds 150
+cls
+Write-Host "Welcom"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome t"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to M"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to Mr"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrR"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRo"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRob"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobo"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobot"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobot'"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobot's"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobot's G"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobot's Gi"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobot's Git"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobot's Gith"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobot's Githu"
+start-sleep -Milliseconds 150
+cls
+write-host "Welcome to MrRobot's Github"
+Write-host
+Write-host
+Write-host
+Write-host
+Write-host
+Write-host
+Write-host
+Write-host
